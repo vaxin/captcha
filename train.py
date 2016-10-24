7229c36074f5f2acd29b2eccea96da7037c7dc8c
root <root@localhost.localdomain>
2016-10-24 17:39:43 +0800

    add autoencode

---
+++ train.py
@@ -0,0 +1,11 @@
+# - coding: utf-8 -
+
+import tensorflow as tf
+import generator
+
+def firstLayer():
+  # input -> conv2d -> output objective: output = input
+
+def trainFirstLayer():
+  training_set = generator.generate()
+
