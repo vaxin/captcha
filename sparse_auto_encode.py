61cdf4e73295ecf79ec597336487afdc2188359d
root <root@localhost.localdomain>
2016-10-25 15:01:48 +0800

    adjust sae

--- sparse_auto_encode.py
+++ sparse_auto_encode.py
@@ -3,28 +3,30 @@ import tensorflow as tf
 import numpy as np
 import util
 
-def train(training_set, img_w, pre_net_op, n_hidden):
+def train(training_set, img_size, pre_net_op, n_hidden):
   '''
   input_data -> pre_net -> layer, the layer can be conv_layer, if not you should make segments by yourself
   '''
-  n_pixel = img_w ** 2
+  img_w = img_size[0]
+  img_h = img_size[1]
+  n_pixel = img_w * img_h
 
   # conv input size
   conv_size = 5
 
   # convert training_set to segment
   new_set = []
-  training_set = training_set.reshape(-1, img_w, img_w)
+  training_set = training_set.reshape(-1, img_w, img_h)
 
   for img in training_set:
-    for item in util.explode(img, conv_size, 1):
+    for item in util.explode(img, conv_size, 10):
       item = np.asarray(item).reshape(conv_size * conv_size)
       new_set.append(item)
   training_set = np.asarray(new_set)
   print training_set.shape
 
-  learning_rate = 1e-5
-  training_epochs = 2000
+  learning_rate = 1e-3
+  training_epochs = 1500
   batch_size = 1000
 
   # layer will trained by autoencoder
@@ -33,12 +35,10 @@ def train(training_set, img_w, pre_net_op, n_hidden):
   X = tf.placeholder(tf.float32, [ None, n_input ])
 
   encoder_W = tf.Variable(tf.random_normal([ n_input, n_hidden ]))
-  encoder_b = tf.Variable(tf.random_normal([ n_hidden ]))
   decoder_W = tf.Variable(tf.random_normal([ n_hidden, n_input ]))
-  decoder_b = tf.Variable(tf.random_normal([ n_input ]))
 
-  mid = tf.nn.sigmoid(tf.matmul(X, encoder_W) + encoder_b)
-  output = tf.nn.sigmoid(tf.matmul(mid, decoder_W) + decoder_b)
+  mid = tf.nn.sigmoid(tf.matmul(X, encoder_W))
+  output = tf.nn.sigmoid(tf.matmul(mid, decoder_W))
 
   beta = tf.constant(3.)
   sparsity = tf.constant(0.01)
@@ -54,8 +54,7 @@ def train(training_set, img_w, pre_net_op, n_hidden):
   # regularization
   regularization = 0.5 * weight_decay * (tf.reduce_sum(tf.pow(encoder_W, 2)) + tf.reduce_sum(tf.pow(decoder_W, 2)))
 
-  cost = square_error + sparsity_penalty #+ regularization
-  #cost = sparsity_penalty
+  cost = square_error + sparsity_penalty + regularization
   optimizer = tf.train.RMSPropOptimizer(learning_rate).minimize(cost)
   
   # it seems the checker has bug for watching gradient descent for tf.pow 
@@ -95,12 +94,19 @@ def train(training_set, img_w, pre_net_op, n_hidden):
     w1 = w.transpose()
     print w1
 
-  return (encoder_W, encoder_b)
+  return encoder_W
 
 if __name__ == "__main__":
+  '''
   import generator
   w = 10
-  n_feature = 50
+  n_feature = 25
   training_set = generator.generate((w, w))
+  '''
+  img = util.gray(util.getImg('desk.jpg'))
+  img_size = img.size
+  img = util.getImageData(img)
+  training_set = np.asarray([ img ])
+  n_feature = 25
   
-  (encoder_W, encoder_b) = train(training_set, w, None, n_feature)
+  encoder_W = train(training_set, img_size, None, n_feature)
