f015f5ca1a0dd68f29bf691a21ae8e4018f4bb6f
vaxin <dengfanxin@gmail.com>
2016-10-28 11:29:50 +0800

    add char images generator add char sequence images generator add test for char sparse auto encoder

--- generator.py
+++ generator.py
@@ -4,48 +4,73 @@ import random
 import util
 import numpy as np
 import img as img_util
-
+import sys
 # just 0-9a-zA-Z
-def getCharImage(char, font_path, size):
+def getCharImage(char, font, size, rotation):
   # always white background, the network should be able to learn seperating the char from the background
   # in fact, the NN should only care about the contrast and not the white backgroud and black font.
   # the input channels to the image will be four channels (Red, Green, Blue, Illumination)
-  txt = Image.new('RGBA', (size, size), (255, 255, 255, 255))
-  font = ImageFont.truetype(font_path, size)
+  txt = Image.new('RGBA', (size, size), (255, 255, 255, 0))
+  font = ImageFont.truetype('fonts/' + font + '.ttf', size)
 
   draw = ImageDraw.Draw(txt)
-  draw.text((0, 0), char, font=font, fill = (0, 0, 0, 255))
+  draw.text((size / 3, 0), char, font=font, fill = (0, 0, 0, 255))
   
-  return txt
+  txt = txt.rotate(rotation, expand = False)
+  bg = Image.new('RGBA', (size, size), (255, 255, 255, 255))
+  bg.paste(txt, (0, 0), txt)
+  return bg
 
-def generate(size):
-  fonts = [ 'simhei' ]
+fonts = [ 'simhei' ]
+chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+def generateChars(size, count = None):
   font_len = len(fonts)
-  chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
   char_len = len(chars)
   ret = []
+  n = 0
   for font in fonts:
     for char in chars:
-      img = getCharImage(chars[random.randint(0, char_len - 1)], 'fonts/' + fonts[random.randint(0, font_len - 1)] + '.ttf', size)
-      
       # rotate 100 times
-      for i in range(100):
+      for i in range(20):
         # rotate
+        img = getCharImage(char, font, size, rotation = random.randint(-60, 60))
         ix = img_util.ImageX(img)
-        ix.rotate(random.randint(-60, 60))
-        ix.save('test.png')
         ix.gray()
         data = ix.getArray()
-        print data.tolist()
+        ret.append(data)
+        n += 1
+        if count is not None and n > count:
+          return np.asarray(ret)
 
-        tt = Image.fromarray(data)
-        tt.save('test.tiff')
-        break
-      break
-    break
+  return np.asarray(ret)
 
+def generateSequenceImage(seq, size, font):
+  bg = Image.new('RGBA', (len(seq) * size, size), (255,) * 4)
+  for i in range(len(seq)):
+    char = seq[i]
+    one = getCharImage(char, font, size, rotation = random.randint(-60, 60))
+    bg.paste(one, (i * size, 0), one)
+  return bg
+
+def generateSequences(count, size, len_range):
+  ''' 
+  generate sequences with random length under len_range
+  The image size will be also random but mainly the type of long strip.
+  '''
+
+  len = random.randint(len_range[0], len_range[1])
+  ret = []
+  for _ in range(count):
+    sequence = np.random.choice(list(chars), len)
+    img = generateSequenceImage(sequence, size, fonts[0] )
+    ix = img_util.ImageX(img)
+    ix.gray()
+    data = ix.getArray()
+    ret.append(data)
   return np.asarray(ret)
 
 
 if __name__ == '__main__':
-  generate(100)
+  #generateChars(30)
+  generateSequences(1, 50, (10, 20))
+
--- tester.py
+++ tester.py
@@ -4,6 +4,7 @@ import tensorflow as tf
 import img as img_util
 import numpy as np
 import util
+import random
 
 def genOneLineGrayImageArray(S):
   return line.genImage(S) / 255.
@@ -52,31 +53,44 @@ def computeCost():
   xs = line
   print sess.run(cost, feed_dict = { X: xs })
 
+def pick(arr, count):
+  ''' randomly pick up n elements from a list '''
+  l = len(arr)
+  ret = []
+  for _ in range(count):
+    ret.append(arr[random.randint(0, l)])
+  return ret
+
+def convTrain(training_set, img_size, conv_size = 10, stride = 1,  n_feature = 100):
+  training_set = img_util.explode(training_set, img_size, conv_size = conv_size, stride = stride)
+  img_util.saveTIFFsFromArray(pick(training_set, 30), 'source') 
+  training_set = training_set.reshape(-1, conv_size * conv_size)
+  weights = sparse_auto_encode.train(training_set, (conv_size, conv_size), None, n_feature)
+  imgs = util.visualize(weights, (conv_size, conv_size))
+  saveImages(imgs)
+
+
 def testWithImage():
   ix = img_util.ImageX('desk.jpg')
   ix.gray()
   arr = ix.getArray()
   img_size = ix.getSize()
-  conv_size = 30
-  n_feature = 500
 
   training_set = np.asarray([ arr ])
-  training_set = img_util.explode(training_set, img_size, conv_size = conv_size, stride = conv_size)
-
-  #img_util.saveTIFFsFromArray(training_set[0:10], 'source') 
-
-  training_set = training_set.reshape(-1, conv_size * conv_size)
-
-  weights = sparse_auto_encode.train(training_set, (conv_size, conv_size), None, n_feature)
-
-  imgs = util.visualize(weights, (conv_size, conv_size))
-  saveImages(imgs)
+  convTrain(training_set, img_size, conv_size = 30, stride = 30, n_feature = 500)
 
 def testConv():
   img = [ [ 11, 12, 13, 14, 15 ], [ 21, 22, 23, 24, 25 ], [ 31, 32, 33, 34, 35 ] ]
   print img_util.conv(img, 2, 1).tolist()
 
+
+def testWithChars():
+  import generator
+  training_set = generator.generateChars(size = 300, count = 10)
+  convTrain(training_set, img_size = (300, 300), conv_size = 10, stride = 5, n_feature = 100)
+
 #computeCost()
 #testLines()
-testWithImage()
+#testWithImage()
 #testConv()
+testWithChars()
